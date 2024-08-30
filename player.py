0cb8fefe641b2021bc2f795bda22ed6c4e666e07
SmiJa <SmiJa13@gmail.com>
2024-08-29 23:43:15 -0700

    implement cooldown

--- player.py
+++ player.py
@@ -5,6 +5,7 @@ from shot import Shot
 
 class Player(CircleShape):
   containers = ()
+  timer = 0
   def __init__(self, x, y):
     super().__init__(x, y, PLAYER_RADIUS)
     self.position = pygame.Vector2(x, y)
@@ -26,21 +27,21 @@ class Player(CircleShape):
 
   def update(self, dt):
     keys = pygame.key.get_pressed()
+    self.timer -= dt
 
     if keys[pygame.K_a]:
       self.rotate(-dt)
-
     if keys[pygame.K_d]:
       self.rotate(dt)
-
     if keys[pygame.K_w]:
       self.move(dt)
-
     if keys[pygame.K_s]:
-      self.move(-dt)
-    
+      self.move(-dt)    
     if keys[pygame.K_SPACE]:
-      self.shoot()
+      if self.timer > 0:
+        pass
+      else:
+        self.shoot()
 
   def move(self, dt):
     forward = pygame.Vector2(0, 1).rotate(self.rotation)
@@ -49,3 +50,4 @@ class Player(CircleShape):
   def shoot(self):
     new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
     new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOT_SPEED
+    self.timer = PLAYER_SHOOT_COOLDOWN
