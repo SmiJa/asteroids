5438274cbc275c8b20df2ded61f170730d168ad8
SmiJa <SmiJa13@gmail.com>
2024-08-29 19:32:03 -0700

    import, update, and draw asteroid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from player import *
+from asteroid import Asteroid
 
 def main():
   pygame.init()
@@ -9,9 +10,11 @@ def main():
 
   updatable = pygame.sprite.Group()
   drawable = pygame.sprite.Group()
+  asteroids = pygame.sprite.Group()
 
   Player.containers = (updatable, drawable)
-  
+  Asteroid.containers = (asteroids, updatable, drawable)
+
   player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
   
   dt = 0
